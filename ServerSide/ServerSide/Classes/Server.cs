cb75b2f813a2b399761af8f710f39b8187851fbe
naaaah <107448004+naaaahTheDev@users.noreply.github.com>
2023-08-13 12:09:11 +0800

    ServerBuilder
    Official Launch!
    In the official launch you can now create servers with ease, by simply inputting the desired IP and server name. With this feature, you no longer need to program at all! Simply run the ServerSide.exe file.

---
+++ ServerSide/ServerSide/Classes/Server.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ServerSide.Classes
+{
+
+
+    public class Server
+	{
+		// List of connected clients
+		private static List<Socket> connectedClients = new List<Socket>();
+
+		// Parameters
+		public IPAddress ip;
+		public int port = 5000;
+		public string serverName;
+		
+		// Setting IP and Server Name
+		public Server(string serverName, IPAddress ip)
+		{
+			this.ip = ip;
+		}
+		
+		// Setting Port
+		public Server(int port)
+		{
+			this.port = port;
+		}
+
+		// Starting server with current parameters
+		public void StartServer()
+		{
+			try
+			{
+				TcpListener listener = new TcpListener(this.ip, this.port);
+
+				listener.Start();
+
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine($"Server Running on port: {this.port}");
+				Console.WriteLine("Local endpoint: " + listener.LocalEndpoint);
+
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Waiting for connections...");
+
+				while (true)
+				{
+					Socket socket = listener.AcceptSocket();
+					connectedClients.Add(socket);
+
+					Console.ForegroundColor = ConsoleColor.Green;
+					Console.WriteLine("\nNew connection accepted from: " + socket.RemoteEndPoint);
+
+					Thread clientThread = new Thread(() =>
+					{
+						HandleClient(socket);
+					});
+					clientThread.Start();
+
+				}
+			} catch (Exception e)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Error: " + e.Message);
+			}
+			
+		}
+
+		private static void HandleClient(Socket clientSocket)
+		{
+			try
+			{
+				while (true)
+				{
+					byte[] dataBuffer = new byte[8192];
+					// Receive data from the current client
+					int bytesRead = clientSocket.Receive(dataBuffer);
+
+					string jsonString = Encoding.ASCII.GetString(dataBuffer, 0, bytesRead);
+					ChatMessage receivedMessage = JsonSerializer.Deserialize<ChatMessage>(jsonString);
+
+					string username = receivedMessage.username;
+					string message = receivedMessage.message;
+
+					Console.ForegroundColor = ConsoleColor.DarkCyan;
+					Console.Write($"\n[{DateTime.Now.ToString("HH:mm:ss")}] ");
+					Console.ForegroundColor = ConsoleColor.Cyan;
+					Console.Write($"From {username}: ");
+					Console.ForegroundColor = ConsoleColor.White;
+					Console.Write($"{message}\n");
+
+					// Checking if there is more than one client connected
+					if (connectedClients.Count > 0)
+					{
+						List<Socket> tempClients = new List<Socket>(connectedClients);
+						// Remove current client so it doesn't send to itself.
+						tempClients.Remove(clientSocket);
+
+						// Sending to each client except the client sending.
+						foreach (Socket client in tempClients)
+						{
+							ASCIIEncoding encoding = new ASCIIEncoding();
+							client.Send(encoding.GetBytes($"From {username}: {message}"));
+						}
+					}
+					ASCIIEncoding asen = new ASCIIEncoding();
+					clientSocket.Send(asen.GetBytes("\nMessage Sent!\n"));
+
+				}
+			} catch (Exception e)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("\nClient Disconnected Due to Error or Manual Disconnection.\n");
+				// Remove client that disconnected to not cause errors while sending data.
+				connectedClients.Remove(clientSocket);
+			}
+		}
+		
+
+
+	}
+}
--- ServerSide/ServerSide/Program.cs
+++ ServerSide/ServerSide/Program.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Text.Json;
+using ServerSide.Classes;
 
 namespace ServerSide
 {
@@ -13,83 +14,55 @@ namespace ServerSide
 		public static List<Socket> connectedClients = new List<Socket>();
 		public static void Main(string[] args)
 		{
-			try
-			{
-				IPAddress ipAd = IPAddress.Parse("INPUT_IP_HERE");
-				TcpListener listener = new TcpListener(ipAd, 5000);
+			Console.WriteLine("Please setup your server!\n");
+			Console.WriteLine("Enter Server Name: ");
 
-				listener.Start();
+			IPAddress ip = IPAddress.Loopback; // Default IP address
+			string serverName = Console.ReadLine();
+			int port = 0;
 
-				Console.ForegroundColor = ConsoleColor.Yellow;
-				Console.WriteLine("Server Running on port: 5000");
-				Console.WriteLine("Local endpoint: " + listener.LocalEndpoint);
-
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine("Waiting for connections...");
-
-				while (true)
-				{
-					Socket socket = listener.AcceptSocket();
-					connectedClients.Add(socket);
-					Console.ForegroundColor = ConsoleColor.Green;
-					Console.WriteLine("\nNew connection accepted from: " + socket.RemoteEndPoint);
-
-					Thread clientThread = new Thread(() =>
-					{
-						HandleClient(socket);
-					});
-					clientThread.Start();
-				}
-			}
-			catch (Exception ex)
+			if (serverName.Length <= 2)
 			{
 				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine("Error: " + ex.Message);
+				Console.WriteLine("Server name should be at least 3 characters long. Please restart the setup and try again.");
+				return;
 			}
-		}
 
-		private static void HandleClient(Socket clientSocket)
-		{
 			try
 			{
-				while (true)
-				{
-					byte[] dataBuffer = new byte[8192];
-					int bytesRead = clientSocket.Receive(dataBuffer);
-
-					string jsonString = Encoding.ASCII.GetString(dataBuffer, 0, bytesRead);
-					ChatMessage receivedMessage = JsonSerializer.Deserialize<ChatMessage>(jsonString);
-					string username = receivedMessage.username;
-					string message = receivedMessage.message;
-
-					Console.ForegroundColor = ConsoleColor.DarkCyan;
-					Console.Write($"\n[{DateTime.Now.ToString("HH:mm:ss")}] ");
-					Console.ForegroundColor = ConsoleColor.Cyan;
-					Console.Write($"From {username}: ");
-					Console.ForegroundColor = ConsoleColor.White;
-					Console.Write($"{message}\n");
-
-					if (connectedClients.Count > 0)
-					{
-						List<Socket> tempClients = new List<Socket>(connectedClients);
-						tempClients.Remove(clientSocket);
-
-						foreach (Socket client in tempClients)
-						{
-							ASCIIEncoding encoding = new ASCIIEncoding();
-							client.Send(encoding.GetBytes($"From {username}: {message}"));
-						}
-					}
-					ASCIIEncoding asen = new ASCIIEncoding();
-					clientSocket.Send(asen.GetBytes("\nMessage Sent!\n"));
-				}
-			}
-			catch (Exception ex)
+				Console.Write("Enter IP Address that you would like to run the server on: ");
+				ip = IPAddress.Parse(Console.ReadLine());
+			} catch (FormatException e)
 			{
-				Console.ForegroundColor = ConsoleColor.DarkRed;
-				Console.WriteLine("\nClient Disconnected Due to Error or Manual Disconnection.\n");
-				connectedClients.Remove(clientSocket);
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Value entered is not in an IP format. Please restart the setup and try again.");
 			}
+
+			try
+			{
+				Console.Write("\nEnter Server Port (enter 0 for default): ");
+				port = int.Parse(Console.ReadLine());
+
+				if (port == 0)
+				{
+					port = 0;
+				}
+			} catch (FormatException e)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Value entered is not a number. Please restart the setup and try again.");
+			}
+
+			// Starting server
+			Server server = new Server(serverName, ip);
+
+			// Setting port if different port is provided.
+			if (port > 0)
+			{
+				server.port = port;
+			}
+
+			server.StartServer();
 		}
 	}
 }
